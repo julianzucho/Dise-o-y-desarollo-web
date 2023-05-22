8928f1c0b3b41a89c73c31170d5ccbb5b1b4dd3c
julianzucho <julianzucho@gmail.com>
2023-05-22 11:25:10 -0300

    .

--- Ejercio Clase/Clase/java.js
+++ Ejercio Clase/Clase/java.js
@@ -26,22 +26,47 @@ for (var i = 0; i < lista.length; i++) {
 
 
 
-var plata=document.querySelector("button")
-/*var miami=document.querySelector("button")
-var ricky=document.querySelector("button")
-var imgplata=document.querySelector("image")
+var plata = document.querySelector("#plata")
+var plata2= document.querySelector("#plata2")
 
-var seEjecutaEnEvento=function(callback){
-imgplata.classList.toggle("oculto")
+var miami = document.querySelector("#miami")
+var miami2= document.querySelector("#miami2")
 
+var ricky = document.querySelector("#ricky")
+var ricky2= document.querySelector("#ricky2")
+
+var ocultop=function(){
+    plata2.classList.toggle("oculto")
 }
 
-plata.addEventListener("click",seEjecutaEnEvento)
-*/
-var lista =document.querySelectorAll("img")
-
-for (var i = 0; i < lista.length; i++) {
-    plata.addEventListener("click",function(callback){
-        this.classList.toggle("highlight")
-    })
+var ocultom=function(){
+    miami2.classList.toggle("oculto")
 }
+
+var ocultor=function(){
+    ricky2.classList.toggle("oculto")
+}
+
+
+ricky.addEventListener("click",ocultor)
+ricky2.addEventListener("click",ocultor)
+
+plata.addEventListener("click",ocultop)
+plata2.addEventListener("click",ocultop)
+
+miami.addEventListener("click",ocultom)
+miami2.addEventListener("click",ocultom)
+
+
+
+
+var magic=document.querySelector("#magic")
+
+magic.addEventListener("mouseover",function(){
+    this.classList.add("oculto")
+})
+
+magic.addEventListener("mouseleave",function(){
+
+    this.classList.remove("oculto")
+})
